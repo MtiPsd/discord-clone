8848819c649a7211d3340bf90d8e0894bdcaefbe
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-19 22:38:29 +0330

    Start :: creating models with  prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+        <body
+          className={cn(font.className, " flex bg-white dark:bg-[#313338]")}
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
