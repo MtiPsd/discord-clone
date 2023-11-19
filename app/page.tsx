6c1dceb54c4135fadee855bfb08b20e9bfdad2a7
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-19 17:22:39 +0330

    Completed :: working on base structure of project

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <p className="text-3xl font-bold text-indigo-600">hello world</p>
-    </div>
-  );
-}
