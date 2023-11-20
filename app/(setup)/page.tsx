6ebc08e407754a79c75cb39b41fa5639973bb06b
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-20 19:36:12 +0330

    Added :: dialog component

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,9 +1,24 @@
+import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
 
 async function SetupPage() {
   const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-  return <div>create a Server</div>;
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a Server</div>;
 }
 
 export default SetupPage;
